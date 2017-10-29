aabc0095f3a8ebf2b32f9847a5d08649849628d2
GodefroiRoussel <godefroi.roussel@gmail.com>
2017-10-29 10:33:39 +0100

    [Doc] CalculSatisfaction

--- groupe.py
+++ groupe.py
@@ -15,7 +15,20 @@ class Groupe:
         """ Fonction calculant la satisfaction de ce groupe
         @In :
         @Out :
+        L'ordre sera le suivant :
+            AR = 0      Toutes les combinaisons avec AR
+            I = 1       Toutes les combinaisons avec I
+            P = 2       P + P
+            P+ = 3      AB + P
+            AB- = 4     B + P       ||  TB + P
+            AB = 5      AB + AB
+            AB+ = 6     B + AB
+            B- = 7      TB + AB
+            B = 8       B + B
+            B+ = 9      TB + B
+            TB = 10     TB + TB
         """
+        
 
 
     def getEleves(self):
